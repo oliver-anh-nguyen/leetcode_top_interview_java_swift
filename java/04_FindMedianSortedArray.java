0a7acfcd26e95e719ece9a11264524ba5ab044f3
tuan.anh.nguyen <oliver.anh.nguyen@gmail.com>
2022-05-02 14:01:58 -0500

    [Leetcode] add 04

---
+++ java/04_FindMedianSortedArray.java
@@ -0,0 +1,12 @@
+public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    int[] mergedArray = new int[nums1.length + nums2.length];
+    System.arraycopy(nums1, 0, mergedArray, 0, nums1.length);
+    System.arraycopy(nums2, 0, mergedArray, nums1.length, nums2.length);
+    Arrays.sort(mergedArray);
+    int middle = mergedArray.length / 2;
+    if (mergedArray.length % 2 == 0) {
+        return (mergedArray[middle] + mergedArray[middle-1]) / 2.0;
+    } else {
+        return mergedArray[middle];
+    }
+}
---
+++ swift/04_FindMedianSortedArray.swift
@@ -0,0 +1,9 @@
+func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
+    let mergedArr = (nums1 + nums2).sorted()
+    let middle = mergedArr.count / 2
+    if mergedArr.count % 2 == 0 {
+        return (Double(mergedArr[middle]) + Double(mergedArr[middle - 1])) / 2
+    } else {
+        return Double(mergedArr[middle])
+    }
+}
